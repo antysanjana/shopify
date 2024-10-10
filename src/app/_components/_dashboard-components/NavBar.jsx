e3e875861b6056d1c123ec56a41fa5fdb8756144
Sanjana Anty <sanjana.anty@gmail.com>
2024-10-10 19:37:34 +0600

    Fixed gitignore

--- src/app/_components/_dashboard-components/NavBar.jsx
+++ src/app/_components/_dashboard-components/NavBar.jsx
@@ -11,53 +11,81 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="relative bg-primary">
-      <div className="container mx-auto flex justify-between items-center py-4 px-6 max-w-screen-xl">
-        <div className="flex items-center justify-between">
-          <a href="/">
-            <Image className="mainlogo" src="/logo.png" alt="Logo" />
-          </a>
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              className="text-gray-500"
-              aria-label="Toggle menu"
-              onClick={toggleMenu}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2"
+    <nav className="relative bg-[#ecf4ff]">
+      <div className="mx-auto flex justify-between items-center py-4 max-w-screen-xl w-10/12">
+        <div className="flex w-screen items-center justify-between">
+          <Link href="/">
+            <Image src="/logo.png" alt="Logo" width={72} height={72} />
+          </Link>
+          <div className="flex border justify-between lg:hidden">
+            {isOpen ? null : (
+              <button
+                type="button"
+                className="text-gray-500"
+                aria-label="Toggle menu"
+                onClick={toggleMenu}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 8h16M4 16h16"
-                />
-              </svg>
-            </button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 8h16M4 16h16"
+                  />
+                </svg>
+              </button>
+            )}
           </div>
         </div>
         <div
-          className={`lg:flex lg:items-center lg:space-x-4 ${
-            isOpen ? "flex" : "hidden"
-          } flex-col lg:flex-row`}
+          className={`absolute z-20 w-screen px-6 py-4 duration-300 ease-in-out translate-x-0 opacity-100 bg-gray-500 flex flex-col p-4 lg:items-center lg:space-x-4   ${
+            isOpen ? "flex flex-col" : "hidden"
+          }lg:flex lg:flex-row`}
         >
-          <Link aria-current="page" className="nav-link" href="/">
-            Home
-          </Link>
-          <Link className="nav-link" href="/about-us">
-            About
-          </Link>
-          <Link className="nav-link" href="/reviews">
-            Reviews
-          </Link>
-          <Link className="nav-link" href="/pricing">
-            Pricing
-          </Link>
+          <div className="flex justify-between">
+            <div className="flex flex-col">
+              <Link aria-current="page" className="nav-link" href="/">
+                Home
+              </Link>
+              <Link className="nav-link" href="/about-us">
+                About
+              </Link>
+              <Link className="nav-link" href="/reviews">
+                Reviews
+              </Link>
+              <Link className="nav-link" href="/pricing">
+                Pricing
+              </Link>
+            </div>
+            <div className="lg-hidden">
+              <button
+                type="button"
+                aria-label="Toggle menu"
+                onClick={toggleMenu}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
@@ -66,47 +94,6 @@ const Navbar = () => {
 
 export default Navbar;
 
-// function NavBar() {
-//   const showNavMenu = () => {};
-
-//   return (
-//     <div className="relative bg-[#ecf4ff]">
-//       <div className="flex max-w-screen-xl py-4 mx-auto items-center justify-between">
-//         <div>
-//           <Image src={"/logo1.png"} alt="logo" width={110} height={110}></Image>
-//         </div>
-//         <div className="lg:hidden">
-//           <button
-//             type="button"
-//             className="text-gray-500"
-//             onClick={showNavMenu}
-//             // aria-label="toggle menu"
-//           >
-//             <svg
-//               xmlns="http://www.w3.org/2000/svg"
-//               className="w-6 h-6"
-//               fill="none"
-//               viewBox="0 0 24 24"
-//               stroke="currentColor"
-//               strokeWidth="2"
-//             >
-//               <path
-//                 strokeLinecap="round"
-//                 strokeLinejoin="round"
-//                 d="M4 8h16M4 16h16"
-//               ></path>
-//             </svg>
-//           </button>
-//         </div>
-//         <div className="hidden lg:flex lg:gap-3 lg:text-lg">
-//           <Link href={"/"}>Home</Link>
-//           <Link href={"/"}>About</Link>
-//           <Link href={"/"}>Reviews</Link>
-//           <Link href={"/"}>Pricing</Link>
-//         </div>
-//       </div>
-//     </div>
-//   );
+// ${
+//   isOpen ? "flex" : "hidden"
 // }
-
-// export default NavBar;
